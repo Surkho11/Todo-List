3b8b5d00f916eb619e1f818b16ca2bbb48dcb848
Surkho <abuyev1999@internet.ru>
2023-06-18 17:12:13 +0300

    Formatting the markup for the new task

--- js/main.js
+++ js/main.js
@@ -11,4 +11,18 @@ form.addEventListener('submit', function (event) {
 	const taskText = taskInput.value;
 
 	// Форматируем разметку для новой задачи
+	const taskHTML = `
+				<li class="list-group-item d-flex justify-content-between task-item">
+					<span class="task-title">${taskText}</span>
+					<div class="task-item__buttons">
+						<button type="button" data-action="done" class="btn-action">
+							<img src="./img/tick.svg" alt="Done" width="18" height="18">
+						</button>
+						<button type="button" data-action="delete" class="btn-action">
+							<img src="./img/cross.svg" alt="Done" width="18" height="18">
+						</button>
+					</div>
+				</li>
+	`
+	console.log(taskHTML);
 })
