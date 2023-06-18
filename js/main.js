ccaf48fd353386fcea70748797311ed5bcf0f304
Surkho <abuyev1999@internet.ru>
2023-06-18 17:05:32 +0300

    Cancel form submission

---
+++ js/main.js
@@ -0,0 +1,10 @@
+// Находим элементы на странице
+
+const form = document.querySelector('#form');
+const taskInput = document.querySelector('#taskInput');
+
+form.addEventListener('submit', function (event) {
+	// отменяем отправку формы
+	event.preventDefault();
+	
+})
